9cd5039c67a005d9f162c5d03b0dcfb8b2b5215e
Kris-Peach <zhozefi@yandex.ru>
2018-04-10 23:17:38 +0300

    update files

--- src/X86.ml
+++ src/X86.ml
@@ -80,98 +80,118 @@ let show instr =
 open SM
 
 (* Symbolic stack machine evaluator
-
      compile : env -> prg -> env * instr list
-
    Take an environment, a stack machine program, and returns a pair --- the updated environment and the list
    of x86 instructions
 *)
-let compare_op cop =
-    match cop with
-    | "<" -> "l"
-    | ">" -> "g"
-    | "<=" -> "le"
-    | ">=" -> "ge"
-    | "==" -> "e"
-    | "!=" -> "ne"
-
-
-let rec compile env code = 
-       	let compile_eval env asm =
-		match asm with
-		| CONST n -> 
-			let e, env = env#allocate in 
-			env, [Mov (L n, e)]
-		| WRITE -> 
-			let e, env = env#pop in 
-			env, [Push e; Call "Lwrite"; Pop eax]
-		| LD x -> 
-			let e, env = (env#global x)#allocate in 
-			env, [Mov (M (env#loc x), eax); Mov (eax, e)]	
-		| ST x -> 
-			let e, env = (env#global x)#pop in 
-			env, [Mov (e, eax); Mov (eax, M (env#loc x))]     
-		| READ -> 
-			let e, env = env#allocate in 
-			env, [Call "Lread"; Mov (eax, e)]
-		 
-		| BINOP operation -> 
-			let sx, sy, env = env#pop2 in 
-            		let e, env = env#allocate in 
-			env, match operation with
-            		| "+" | "-" | "*" -> 
-				[
-				  Mov (sy, eax); 
-				  Binop (operation, sx, eax); 
-				  Mov (eax, sy)
-				]
-			| "/" -> 
-				[
-				  Mov (sy, eax); 
-				  Cltd; 
-				  IDiv sx; 
-				  Mov (eax, e)
-				]
-			| "%" -> 
-				[
-				  Mov (sy, eax); 
-				  Cltd; 
-				  IDiv sx; 
-				  Mov (edx, e)
-				]			
-				
-			| "<" | ">" | "<=" | ">=" | "==" | "!=" ->
-                            let cop = compare_op operation in
-                                [
-                                  Mov (sy, eax);
-                                  Binop ("cmp", sx, eax);
-                                  Mov (eax, sy)] @ [Mov (L 0, eax);
-                                  Set (cop, "%al");
-                                  Mov (eax, e)
-                                ]
-				
-            		| "&&" | "!!" -> 
-				[
-				  Binop ("^", eax, eax); 
-				  Binop ("^", edx, edx);
-                  		  Binop ("cmp", L 0, sx); 
-				  Set ("nz", "%al"); 
-                  		  Binop ("cmp", L 0, sy); 
-				  Set ("nz", "%dl"); 
-                  		  Binop (operation, eax, edx); 
-				  Mov (edx, e)
-				]
-
-			| _ -> failwith "Unknown binary operator" in
-			  
-		
-		match code with
-		| [] -> env, []
-		| instr::t_code ->
-			let n_env , asm = compile_eval env instr in
-			let next_env, asm_list = compile n_env t_code in 
-			next_env, asm @ asm_list
-
+let compile env code =
+  let suffix = function
+  | "<"  -> "l"
+  | "<=" -> "le"
+  | "==" -> "e"
+  | "!=" -> "ne"
+  | ">=" -> "ge"
+  | ">"  -> "g"
+  | _    -> failwith "unknown operator"	
+  in
+  let rec compile' env scode =
+    let on_stack = function S _ -> true | _ -> false in
+    match scode with
+    | [] -> env, []
+    | instr :: scode' ->
+        let env', code' =
+          match instr with
+          | READ ->
+             let s, env' = env#allocate in
+             (env', [Call "Lread"; Mov (eax, s)])               
+          | WRITE ->
+             let s, env' = env#pop in
+             (env', [Push s; Call "Lwrite"; Pop eax])
+  	  | CONST n ->
+             let s, env' = env#allocate in
+	     (env', [Mov (L n, s)])               
+	  | LD x ->
+             let s, env' = (env#global x)#allocate in
+             env',
+	     (match s with
+	      | S _ | M _ -> [Mov (M (env'#loc x), eax); Mov (eax, s)]
+	      | _         -> [Mov (M (env'#loc x), s)]
+	     )	        
+	  | ST x ->
+	     let s, env' = (env#global x)#pop in
+             env',
+             (match s with
+              | S _ | M _ -> [Mov (s, eax); Mov (eax, M (env'#loc x))]
+              | _         -> [Mov (s, M (env'#loc x))]
+	     )
+          | BINOP op ->
+	     let x, y, env' = env#pop2 in
+             env'#push y,
+             (match op with
+	      | "/" | "%" ->
+                 [Mov (y, eax);
+                  Cltd;
+                  IDiv x;
+                  Mov ((match op with "/" -> eax | _ -> edx), y)
+                 ]
+              | "<" | "<=" | "==" | "!=" | ">=" | ">" ->
+                 (match x with
+                  | M _ | S _ ->
+                     [Binop ("^", eax, eax);
+                      Mov   (x, edx);
+                      Binop ("cmp", edx, y);
+                      Set   (suffix op, "%al");
+                      Mov   (eax, y)
+                     ]
+                  | _ ->
+                     [Binop ("^"  , eax, eax);
+                      Binop ("cmp", x, y);
+                      Set   (suffix op, "%al");
+                      Mov   (eax, y)
+                     ]
+                 )
+              | "*" ->
+                 if on_stack x && on_stack y 
+		 then [Mov (y, eax); Binop (op, x, eax); Mov (eax, y)]
+                 else [Binop (op, x, y)]
+	      | "&&" ->
+		 [Mov   (x, eax);
+		  Binop (op, x, eax);
+		  Mov   (L 0, eax);
+		  Set   ("ne", "%al");
+                  
+		  Mov   (y, edx);
+		  Binop (op, y, edx);
+		  Mov   (L 0, edx);
+		  Set   ("ne", "%dl");
+                  
+                  Binop (op, edx, eax);
+		  Set   ("ne", "%al");
+                  
+		  Mov   (eax, y)
+                 ]		   
+	      | "!!" ->
+		 [Mov   (y, eax);
+		  Binop (op, x, eax);
+                  Mov   (L 0, eax);
+		  Set   ("ne", "%al");
+		  Mov   (eax, y)
+                 ]		   
+	      | _   ->
+                 if on_stack x && on_stack y 
+                 then [Mov   (x, eax); Binop (op, eax, y)]
+                 else [Binop (op, x, y)]
+             )
+          | LABEL s     -> env, [Label s]
+	  | JMP   l     -> env, [Jmp l]
+          | CJMP (s, l) ->
+              let x, env = env#pop in
+              env, [Binop ("cmp", L 0, x); CJmp  (s, l)]
+        in
+        let env'', code'' = compile' env' scode' in
+	env'', code' @ code''
+  in
+  compile' env code
 
 (* A set of strings *)           
 module S = Set.Make (String)
@@ -257,4 +277,4 @@ let build stmt name =
   close_out outf;
   let inc = try Sys.getenv "RC_RUNTIME" with _ -> "../runtime" in
   Sys.command (Printf.sprintf "gcc -m32 -o %s %s/runtime.o %s.s" name inc name)
- 
+
